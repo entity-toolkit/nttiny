1e0f1cef717b31970830e7d6038405224636cc03
haykh <haykh.mfs+github@gmail.com>
2022-10-02 19:12:15 -0400

    ⬆️ fs as default

--- nttiny/export.cpp
+++ nttiny/export.cpp
@@ -23,17 +23,8 @@
 
 #undef STB_IMAGE_WRITE_IMPLEMENTATION
 
-#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
-#  if __has_include(<filesystem>)
-#    define GHC_USE_STD_FS
-#    include <filesystem>
-namespace fs = std::filesystem;
-#  endif
-#endif
-#ifndef GHC_USE_STD_FS
-#  include <ghc/filesystem.hpp>
+#include <ghc/filesystem.hpp>
 namespace fs = ghc::filesystem;
-#endif
 
 void saveImage(const std::string& fileName, const std::string& path) {
   ImGuiIO io = ImGui::GetIO();
