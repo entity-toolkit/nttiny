fde78b95be456a1c0b5beaa279219495e8322354
haykh <haykh.mfs+github@gmail.com>
2022-10-03 10:26:13 -0400

    🚚 nttiny -> src

--- nttiny/export.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXPORT_H
-#define EXPORT_H
-
-#include <string>
-
-void saveImage(const std::string&, const std::string& path = "./");
-
-#endif // EXPORT_H
--- nttiny/ui/implot_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef IMPORT_UTILS_H
-#define IMPORT_UTILS_H
-
-#include <implot/implot.h>
-#include <implot/implot_internal.h>
-
-#include "implot_renderers.h"
-
-#endif
